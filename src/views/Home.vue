1ff8416c6f973d9ea37643fa545a168b7893b385
fjliang56 <86655295+fjliang56@users.noreply.github.com>
2021-08-17 11:06:31 +0800

    搭建html结构

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="topnav">
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="banner">
+      <h1>HZW UI</h1>
+      <h2>一个好用的 UI 框架</h2>
+      <p class="actions">
+        <a>Github</a>
+        <a>开始</a>
+      </p>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.topnar {
+  border: none;
+}
+</style>
