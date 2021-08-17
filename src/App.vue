9cc90c185e22aeeb469963540960737ab35412ad
fjliang56 <86655295+fjliang56@users.noreply.github.com>
2021-08-17 15:22:53 +0800

    调节 topnav 和 aside 的位置

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div>
-    导航栏 |
-    <router-link to="/">FFFF</router-link>
-    <router-link to="/xxx">FFF</router-link>
+    <router-link to="/">Home</router-link>
+    <router-link to="/doc">Doc</router-link>
   </div>
-  <hr />
   <router-view />
 </template>
 
