14fbc9f7d8cc879bde671cb022f0b4e8a514558b
fjliang56 <86655295+fjliang56@users.noreply.github.com>
2021-08-18 18:07:16 +0800

    加载css

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -43,11 +43,15 @@
       <Button theme="text" level="danger">危险文字按钮</Button>
     </div>
   </div>
-  <1>示例4</1>
+  <h1>示例4</h1>
   <div>
       <Button disabled>禁用按钮</Button>
       <Button theme="link" disabled>禁用链接按钮</Button>
       <Button theme="text" disabled>禁用按钮</Button>
+  </div>
+  <h1>示例5</h1>
+  <div>
+      <Button loading>加载中</Button>
   </div>
 </template>
 <script lang="ts">
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,6 @@
 <template>
   <button class="hzw-button" :class="classes" :disabled="disabled">
+    <span v-if="loading" class="hzw-loadingTndicator"></span>
     <slot />
   </button>
 </template>
@@ -20,6 +21,10 @@ export default {
       default: "normal",
     },
     disabled: {
+      type: Boolean,
+      default: "false",
+    },
+    loading: {
       type: Boolean,
       default: "false",
     },
@@ -163,6 +168,25 @@ $grey: grey;
       cursor: not-allowed;
       color: $grey;
     }
+  }
+  > .hzw-loadingIndictor {
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: hzw-spin 1s infinite linear;
+  }
+}
+@keyframes hzw-spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
   }
 }
 </style>
