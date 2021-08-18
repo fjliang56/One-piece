2f04436997e415bbf282951e5e7780161bec05f3
fjliang56 <86655295+fjliang56@users.noreply.github.com>
2021-08-18 17:39:14 +0800

    支持level属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -9,19 +9,39 @@
   </div>
   <h1>示例2</h1>
   <div>
+    <div>
       <Button size="big">大大大</Button>
       <Button>普通</Button>
       <Button size="small">小小小</Button>
-  </div>
-  <div>
+    </div>
+    <div>
       <Button theme="link" size="big">大大大</Button>
       <Button theme="link">普通</Button>
       <Button theme="link" size="small">小小小</Button>
-  </div>
-  <div>
+    </div>
+    <div>
       <Button theme="text" size="big">大大大</Button>
       <Button theme="text">普通</Button>
       <Button theme="text" size="small">小小小</Button>
+    </div>
+  </div>
+  <h3>示例3</h3>
+  <div>
+    <div>
+      <Button level="main">主要按钮</Button>
+      <Button>普通</Button>
+      <Button level="danger">危险按钮</Button>
+    </div>
+    <div>
+      <Button theme="link" level="main">主要链接按钮</Button>
+      <Button theme="link">普通</Button>
+      <Button theme="link" level="danger">危险链接按钮</Button>
+    </div>
+    <div>
+      <Button theme="text" level="main">主要文字按钮</Button>
+      <Button theme="text">普通</Button>
+      <Button theme="text" level="danger">危险文字按钮</Button>
+    </div>
   </div>
 </template>
 <script lang="ts">
