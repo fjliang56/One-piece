904cdb9b1328ceca085ce7113b34ce551c4fccc9
fjliang56 <86655295+fjliang56@users.noreply.github.com>
2021-08-18 16:41:23 +0800

    CSS最小影响原则

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="hzw-button" :class="{ [`theme-${theme}`]: theme }">
+  <button class="hzw-button" :class="{ [`hzw-theme-${theme}`]: theme }">
     <slot />
   </button>
 </template>
@@ -13,7 +13,7 @@ export default {
   },
 };
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 $h: 32px;
 $border-color: #d9d9d9;
 $color: #333;
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="toggle" :class="{ checked: value }">
+  <button class="hzw-switch" @click="toggle" :class="{ 'hzw-checked': value }">
     <span> </span>
   </button>
 </template>
@@ -16,10 +16,10 @@ export default {
   },
 };
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 $h: 22px;
 $h2: $h - 4px;
-button {
+.hzw-switch {
   height: $h;
   width: $h * 2;
   border: none;
@@ -36,7 +36,7 @@ button {
     border-radius: $h2/2;
     transition: all 250ms;
   }
-  &.checked {
+  &.hzw-checked {
     background: #1890ff;
     > span {
       left: calc(100% - #{$h2} - 2px);
@@ -51,7 +51,7 @@ button {
       width: $h2 + 4px;
     }
   }
-  &.checked:active {
+  &.hzw-checked:active {
     > span {
       width: $h2 + 4px;
       margin-left: -4px;
