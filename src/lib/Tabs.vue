a47f6405a69d3a21f2745873a20ac4f6a860e4b1
fjliang56 <86655295+fjliang56@users.noreply.github.com>
2021-08-19 16:46:27 +0800

    优化1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -6,7 +6,7 @@
         v-for="(t, index) in titles"
         :ref="
           (el) => {
-            if (el) navItem[index] = el;
+            if (t === selected) selectedItem = el;
           }
         "
         @click="select(t)"
@@ -39,19 +39,14 @@ export default {
     },
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([]);
+    const selectedItem = ref<HTMLDivElement>(null);
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
     const x = () => {
-      const divs = navItems.value;
-      const result = divs.filter((div) =>
-        div.classList.contains("selected")
-      )[0];
-      console.log(result);
-      const { width } = result.getBoundingClientRect();
+      const { width } = selectedItem.value.getBoundingClientRect();
       indicator.value.style.width = width + "px";
       const { left: left1 } = container.value.getBoundingClientRect();
-      const { left: left2 } = result.getBoundingClientRect();
+      const { left: left2 } = selectedItem.value.getBoundingClientRect();
       const left = left2 - left1;
       indicator.value.style.left = left + "px";
     };
@@ -81,7 +76,7 @@ export default {
       titles,
       current,
       select,
-      navItems,
+      selectedItem,
       indicator,
       container,
     };
