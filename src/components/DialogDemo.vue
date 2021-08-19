8d5babe3b5cd9a68d57dbd84698866a8ab0961f2
fjliang56 <86655295+fjliang56@users.noreply.github.com>
2021-08-19 10:09:34 +0800

    创建Diglog

---
+++ src/components/DialogDemo.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>Dialog 示例</div>
+    <1>示例1</1>
+    <Dialog></Dialog>
+</template>
+
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+
+export default {
+    components: {
+        Dialog
+    }
+}
+
+</script>
