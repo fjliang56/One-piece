9bff9120fe4ef542428e96a464090c9f07c0d92a
fjliang56 <86655295+fjliang56@users.noreply.github.com>
2021-08-19 15:49:57 +0800

    Tabs scss 1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
-    <div v-for="(t, index) in titles" :key="index">{{ t }}</div>
-    <component v-for="(c, index) in defaults" :is="c" :key="index" />
+  <div class="hzw-tabs">
+    <div class="hzw-tabs-nav">
+      <div class="hzw-tabs-nav-item" v-for="(t, index) in titles" :key="index">
+        {{ t }}
+      </div>
+    </div>
+    <div class="hzw-tabs-content">
+      <component
+        class="hzw-tabs-content-item"
+        v-for="(c, index) in defaults"
+        :is="c"
+        :key="index"
+      />
+    </div>
   </div>
 </template>
 
@@ -26,3 +37,35 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+
+.hzw-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+
+      &:first-child {
+        margin-left: 0;
+      }
+
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
