84132360688d222c1ee560f2008d9345fd947cc6
fjliang56 <86655295+fjliang56@users.noreply.github.com>
2021-08-19 16:39:12 +0800

    实现下划线滑动

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -3,14 +3,14 @@
   <h1>示例1</h1>
   <Tabs v-model:selected="x">
     <Tab title="导航1">内容1</Tab>
-    <Tab title="导航2">内容2</Tab>
+    <Tab title="导航2222222">内容2</Tab>
   </Tabs>
 </template>
 
 <script lang="ts">
 import Tabs from "../lib/Tabs.vue";
 import Tab from "../lib/Tab.vue";
-import { ref } from 'vue'
+import { ref } from "vue";
 
 export default {
   components: {
@@ -18,10 +18,10 @@ export default {
     Tab,
   },
   setup() {
-      const x = ref('导航1')
-      return {
-          x
-      }
-  }
+    const x = ref("导航2222222");
+    return {
+      x,
+    };
+  },
 };
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="hzw-tabs">
-    <div class="hzw-tabs-nav">
+    <div class="hzw-tabs-nav" ref="container">
       <div
         class="hzw-tabs-nav-item"
         v-for="(t, index) in titles"
+        :ref="
+          (el) => {
+            if (el) navItem[index] = el;
+          }
+        "
         @click="select(t)"
         :class="{ selected: t === selected }"
         :key="index"
       >
         {{ t }}
       </div>
-      <div class="hzw-tabs-nav-indicator"></div>
+      <div class="hzw-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="hzw-tabs-content">
       <component
@@ -25,7 +30,7 @@
 
 <script lang="ts">
 import Tab from "./Tabs.vue";
-import { computed } from "vue";
+import { computed, ref, onMounted, onUpdated } from "vue";
 
 export default {
   props: {
@@ -34,6 +39,25 @@ export default {
     },
   },
   setup(props, context) {
+    const navItems = ref<HTMLDivElement[]>([]);
+    const indicator = ref<HTMLDivElement>(null);
+    const container = ref<HTMLDivElement>(null);
+    const x = () => {
+      const divs = navItems.value;
+      const result = divs.filter((div) =>
+        div.classList.contains("selected")
+      )[0];
+      console.log(result);
+      const { width } = result.getBoundingClientRect();
+      indicator.value.style.width = width + "px";
+      const { left: left1 } = container.value.getBoundingClientRect();
+      const { left: left2 } = result.getBoundingClientRect();
+      const left = left2 - left1;
+      indicator.value.style.left = left + "px";
+    };
+    onMounted(x);
+    onUpdated(x);
+
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
@@ -57,6 +81,9 @@ export default {
       titles,
       current,
       select,
+      navItems,
+      indicator,
+      container,
     };
   },
 };
@@ -95,6 +122,7 @@ $border-color: #d9d9d9;
       left: 0;
       bottom: -1px;
       width: 100px;
+      transition: all 250ms;
     }
   }
 
