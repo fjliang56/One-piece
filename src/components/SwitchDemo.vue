ab937b5aba90dc9684d0844e518dd0e483be9d43
fjliang56 <86655295+fjliang56@users.noreply.github.com>
2021-08-20 17:30:14 +0800

    删除无用代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -7,26 +7,20 @@
 </template>
 
 <script lang="ts">
-import Switch from "../lib/Switch.vue";
 import { ref } from "vue";
-import Button from "../lib/Button.vue";
 import Switch1Demo from "./Switch1.demo.vue";
 import Switch2Demo from "./Switch2.demo.vue";
-import "prismjs";
-const Prism = (window as any).Prism;
 import Demo from "./Demo.vue";
 
 export default {
-  components: { Button, Demo },
+  components: { Demo },
   setup() {
-    const bool = ref(false);
-    return { bool, Switch1Demo, Switch2Demo, Prism };
+    return { Switch1Demo, Switch2Demo };
   },
 };
 </script>
 
 <style lang="scss">
-@import "prismjs/themes/prism.css";
 $border-color: #d9d9d9;
 .demo {
   border: 1px solid $border-color;
