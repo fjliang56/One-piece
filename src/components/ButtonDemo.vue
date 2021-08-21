1b9b480454d72119e06eafb235c1df0f6ee3cc71
fjliang56 <86655295+fjliang56@users.noreply.github.com>
2021-08-21 16:00:52 +0800

    添加Button描述

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -6,6 +6,7 @@
     <Demo :component="Button3Demo" />
     <Demo :component="Button4Demo" />
     <Demo :component="Button5Demo" />
+    <Attr :columns="columns" :data="data" />
   </div>
 </template>
 <script lang="ts">
@@ -15,10 +16,60 @@ import Button2Demo from "./Button2.demo.vue";
 import Button3Demo from "./Button3.demo.vue";
 import Button4Demo from "./Button4.demo.vue";
 import Button5Demo from "./Button5.demo.vue";
+import { ref } from "vue";
+import Attr from "./Attr.vue";
+import { columns } from "../lib/data";
+import DocTitle from "./DocTitle.vue";
+
 export default {
-  components: { Demo },
+  components: { Demo, Attr, DocTitle },
   setup() {
-    return { Button1Demo, Button2Demo, Button3Demo, Button4Demo, Button5Demo };
+    const data = ref([
+      {
+        params: "normal",
+        desc: "常规使用",
+        type: "string",
+        select: "normal / FontColor / Shadow",
+        default: "default",
+      },
+      {
+        params: "size",
+        desc: "尺寸大小",
+        type: "string",
+        select: "small / normal / large",
+        default: "normal",
+      },
+      {
+        params: "level",
+        desc: "不同形式",
+        type: "string",
+        select: "normal / link / text",
+        default: "normal",
+      },
+      {
+        params: "disabled",
+        desc: "禁止",
+        type: "string",
+        select: "border / FontColor / Shadow",
+        default: "normal",
+      },
+      {
+        params: "loading",
+        desc: "是否加载",
+        type: "boolean",
+        select: "false / true",
+        default: "false",
+      },
+    ]);
+    return {
+      data,
+      columns,
+      Button1Demo,
+      Button2Demo,
+      Button3Demo,
+      Button4Demo,
+      Button5Demo,
+    };
   },
 };
 </script>
