53f46f1897cc54cd02756198b5640d0c5e14f184
fjliang56 <86655295+fjliang56@users.noreply.github.com>
2021-08-21 16:22:25 +0800

    添加Tabs描述

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,19 +1,40 @@
 <template>
   <h1>Tabs 示例</h1>
   <Demo :component="Tabs1Demo" />
+  <Attr :columns="columns" :data="data" />
 </template>
 
 <script lang="ts">
 import Demo from "./Demo.vue";
 import Tabs1Demo from "./Tabs1.demo.vue";
+import Attr from "./Attr.vue";
+import { columns } from "../lib/data";
+import DocTitle from "./DocTitle.vue";
+import { ref } from "vue";
 
 export default {
   components: {
     Demo,
+    Attr,
+    DocTitle,
+    Tabs1Demo,
   },
   setup() {
+    const data = ref([
+      {
+        params: "tab-item",
+        desc: "tab页签",
+        type: "component",
+        select: "tab-item",
+        default: "--",
+      },
+    ]);
+    const title = ref("学习");
     return {
       Tabs1Demo,
+      data,
+      columns,
+      title,
     };
   },
 };
