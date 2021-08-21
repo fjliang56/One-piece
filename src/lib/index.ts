ffed752674476e75666119b1b41b80c0238616e5
fjliang56 <86655295+fjliang56@users.noreply.github.com>
2021-08-21 21:05:36 +0800

    创建lib/index.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,9 @@
+export { default as Switch } from "./Switch.vue";
+export { default as Button } from "./Button.vue";
+export { default as Tabs } from "./Tabs.vue";
+export { default as Tab } from "./Tab.vue";
+export { default as Input } from "./Input.vue";
+export { default as Radio } from "./Radio.vue";
+export { default as Dialog } from "./Dialog.vue";
+export { openDialog as openDialog } from "./openDialog";
+export { columns as columns } from "./data";
